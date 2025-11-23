9def5f4df2418332755c11743608fea4e5f84db5
ashok9109 <aaaaashokyadav123@gmail>
2025-11-23 20:00:21 +0530

    MongoDb is connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
 const app = require("./src/app");
+const connectDb = require("./src/config/db/db")
 
+connectDb();
 app.listen(3000, ()=>{
     console.log("server is runing port 3000");
 })
---
+++ backend/src/config/db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDb = async()=>{
+    try {
+        const res = await mongoose.connect(process.env.MONGOOSE_URI)
+        if(res){
+            console.log("MongoDB is connected successfully");
+        }
+        
+    } catch (error) {
+        console.log("Error in connecting mongoDb");
+    };
+};
+
+module.exports = connectDb;
